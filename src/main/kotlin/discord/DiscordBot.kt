46e8da713268cff49db307c54c9abe8782b9c9fc
Dae Lorant <DLorant@gmail.com>
2023-12-13 18:58:26 -0800

    First cut of both that works but doesn't use slash commands

--- src/main/kotlin/discord/DiscordBot.kt
+++ src/main/kotlin/discord/DiscordBot.kt
@@ -3,43 +3,49 @@ package discord
 import generateTextScript
 import getScriptMetadata
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.OptionType.STRING
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.utils.FileUpload
 
 class DiscordBot(private val token: String) : ListenerAdapter() {
 
   fun start() {
-    val jda = JDABuilder.createDefault(token)
-      .addEventListeners(this)
-      .build()
+    val jda = JDABuilder.createDefault(token).addEventListeners(this).build()
+    jda.upsertCommand(
+      Commands.slash(
+        "textscript", "Generate a 5oS style text script from a JSON file"
+      ).addOption(STRING, "input", "The input script in JSON format", true)
+    ).queue()
     jda.awaitReady()
     println("Bot is ready!")
   }
 
-  override fun onMessageReceived(event: MessageReceivedEvent) {
-    val message: Message = event.message
-    val content: String = message.contentDisplay
+  override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+    if (event.name == "textscript") {
+      handleTextScript(event)
+    }
+  }
+
+  private fun handleTextScript(event: SlashCommandInteractionEvent) {
+    event.deferReply().queue()
     val channel: MessageChannel = event.channel
-    val user: User = event.author
-
-    if (user.isBot) return
-
-    if (content.startsWith("!generateScript")) {
-      val json = content.substringAfter(" ")
-      val scriptMetadata = getScriptMetadata(json)
-      val output = generateTextScript(scriptMetadata, json)
+    val scriptJson = event.options[0].asString
+    try {
+      val scriptMetadata = getScriptMetadata(scriptJson)
+      val output = generateTextScript(scriptMetadata, scriptJson)
 
       // Send the InputStream as an attachment
       channel.sendFiles(
         FileUpload.fromData(
-          output.toByteArray(),
-          "${scriptMetadata?.name ?: "output"}.md"
+          output.toByteArray(), "${scriptMetadata?.name ?: "output"}.md"
         )
       ).queue()
+      event.hook.sendMessage(scriptJson).queue()
+    } catch (e: Exception) {
+      event.hook.sendMessage("Invalid JSON: $scriptJson").queue()
     }
   }
 }
