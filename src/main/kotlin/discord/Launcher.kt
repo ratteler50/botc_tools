3f4f4ee097ec415c4cbb408e89a9da42f7ae6fa3
Dae Lorant <DLorant@gmail.com>
2023-12-14 19:20:09 -0800

    Take in tokens from the command line, and support multiple bots by passing multiple tokens.

--- src/main/kotlin/discord/Launcher.kt
+++ src/main/kotlin/discord/Launcher.kt
@@ -1,9 +1,26 @@
 package discord
 
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
+
 object Launcher {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val bot = DiscordBot(Settings.BOT_TOKEN)
-        bot.start()
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val parser = ArgParser("discordBot")
+    val botTokens by parser.option(
+      ArgType.String,
+      shortName = "t",
+      description = "The bot tokens, separated by comma"
+    ).required()
+
+    parser.parse(args)
+
+    val tokens = botTokens.split(",")
+
+    tokens.forEach { token ->
+      val bot = DiscordBot(token.trim())
+      bot.start()
     }
+  }
 }
