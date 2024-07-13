50d8a69f6164797bf528d39b663fff9702122a6a
Dae Lorant <DLorant@gmail.com>
2024-07-13 14:32:01 -0700

    Read tokens from a tokens file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Read TOKEN from token.txt file
+TOKEN=$(cat token.txt)
 # Use exec to replace the shell with the java process
 exec java -cp target/consoleApp-1.0-SNAPSHOT-jar-with-dependencies.jar discord.Launcher -t "${TOKEN}"
