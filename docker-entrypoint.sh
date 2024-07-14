0b8b7fc3234ebcbd6bbdf95b6ee47997f01d99b4
Dae Lorant <DLorant@gmail.com>
2024-07-14 04:08:05 -0700

    Updated pom to no longer create a deploy jar and to configure classpath automagically via mvn.  Updated docker-entrypoint.sh to match.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 # Read TOKEN from token.txt file
 TOKEN=$(cat token.txt)
 # Use exec to replace the shell with the java process
-exec java -cp target/consoleApp-1.0-SNAPSHOT-jar-with-dependencies.jar discord.Launcher -t "${TOKEN}"
+exec java -jar target/botcTools-1.0-SNAPSHOT.jar -t "${TOKEN}"
